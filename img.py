a56a5d617629c1f434002163d9d7556eec11e39f
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2021-12-23 21:03:05 +0800

    Update img.py

--- img.py
+++ img.py
@@ -7,6 +7,27 @@ import tensorflow_addons as tfa
 import numpy as np
 from autocrop import Cropper
 
+
+     Image = st.file_uploader('Upload your portrait here',type=['jpg','jpeg','png'])
+        if Image is not None:
+            col1, col2 = st.beta_columns(2)
+            Image = Image.read()
+            Image = tf.image.decode_image(Image, channels=3).numpy()                  
+            Image = adjust_gamma(Image, gamma=gamma)
+            with col1:
+                st.image(Image)
+            input_image = loadtest(Image,cropornot=Autocrop)
+            prediction = comic_model(input_image, training=True)
+            prediction = tf.squeeze(prediction,0)
+            prediction = prediction* 0.5 + 0.5
+            prediction = tf.image.resize(prediction, 
+                           [outputsize, outputsize],
+                           method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+            prediction=  prediction.numpy()
+            with col2:
+                st.image(prediction)
+
+
 def adjust_gamma(image, gamma=1.0):
     invGamma = 1.0 / gamma
     table = np.array([((i / 255.0) ** invGamma) * 255
