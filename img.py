1d78b282e4792c405414bc69955519215dbc47a5
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2021-12-23 21:56:51 +0800

    Update img.py

--- img.py
+++ img.py
@@ -2,7 +2,4 @@ import streamlit as st
 Image = st.file_uploader('Upload your portrait here',type=['jpg','jpeg','png'])
    if Image is not None:
         Image = Image.read()
-            Image = tf.image.decode_image(Image, channels=3).numpy()                  
-            Image = adjust_gamma(Image, gamma=gamma)
-            with col1:
-                st.image(Image)
+       
