a2d55f33d025246a2caa8fb8dd5f90d2054b61c9
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2021-12-23 21:04:47 +0800

    Update img.py

--- img.py
+++ img.py
@@ -1,11 +1,6 @@
 import streamlit as st
 import cv2
-import tensorflow as tf
-import io
-from PIL import Image
-import tensorflow_addons as tfa
-import numpy as np
-from autocrop import Cropper
+
 
 
      Image = st.file_uploader('Upload your portrait here',type=['jpg','jpeg','png'])
