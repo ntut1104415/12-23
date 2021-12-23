e0dcc6e5375b04489b16a15133c0621e388fefff
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2021-12-23 21:05:29 +0800

    Update img.py

--- img.py
+++ img.py
@@ -2,9 +2,8 @@ import streamlit as st
 import cv2
 
 
-
-     Image = st.file_uploader('Upload your portrait here',type=['jpg','jpeg','png'])
-        if Image is not None:
+Image = st.file_uploader('Upload your portrait here',type=['jpg','jpeg','png'])
+      if Image is not None:
             col1, col2 = st.beta_columns(2)
             Image = Image.read()
             Image = tf.image.decode_image(Image, channels=3).numpy()                  
