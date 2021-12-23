e083862bac28bf0998c8842fb06674f63ee5e21e
ntut1104415 <95857687+ntut1104415@users.noreply.github.com>
2021-12-23 21:36:51 +0800

    Update img.py

--- img.py
+++ img.py
@@ -1,5 +1,21 @@
 import streamlit as st
 
 
-Image = st.file_uploader('Upload your portrait here',type=['jpg','jpeg','png'])
- st.image(Image)
+ Image = st.file_uploader('Upload your portrait here',type=['jpg','jpeg','png'])
+        if Image is not None:
+            col1, col2 = st.beta_columns(2)
+            Image = Image.read()
+            Image = tf.image.decode_image(Image, channels=3).numpy()                  
+            Image = adjust_gamma(Image, gamma=gamma)
+            with col1:
+                st.image(Image)
+            input_image = loadtest(Image,cropornot=Autocrop)
+            prediction = comic_model(input_image, training=True)
+            prediction = tf.squeeze(prediction,0)
+            prediction = prediction* 0.5 + 0.5
+            prediction = tf.image.resize(prediction, 
+                           [outputsize, outputsize],
+                           method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+            prediction=  prediction.numpy()
+            with col2:
+                st.image(prediction)
